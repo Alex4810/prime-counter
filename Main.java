b7742f15f993d4e1dedc2b8c02bc3fe4ba52fdd9
Alex <alexanderlinus@gmail.com>
2023-09-02 21:56:22 -0700

    final program

--- Main.java
+++ Main.java
@@ -1,20 +1,19 @@
+import java.io.IOException;
 import java.util.Scanner;
 
-
+//main file
 public class Main
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
         System.out.println("Please input number:\n");
         int input = scan.nextInt();
         countPrimes(input);
-        System.out.println("\nThere are " + count + " prime numbers under " + input + ".");
 
     }
 
     public static boolean isPrime(int num)
-        {
+    {
         if(num == 2 )
         {
             return true;
@@ -24,25 +23,33 @@ public class Main
             return false;
         }
         else for (int i = 2; i < Math.sqrt(num) + 1; i++)
-        {
-            if(num%i == 0)
             {
-                return false;
+                if(num%i == 0)
+                {
+                    return false;
+                }
             }
-        }
         return true;
     }
     public static int count = 0;
 
-    public static void countPrimes(int num) {
+    public static void countPrimes(int num) throws IOException {
+        WriteFile writeFile = new WriteFile("primeNumbers.txt");
+        writeFile.openFile();
         for(int i = 2; i < num; i ++)
         {
             if(isPrime(i))
             {
                 count++;
-                System.out.println(count + ". " + i);
+                String primeNumber = count + ". " + i;
+                System.out.println(primeNumber);
+                writeFile.writeToFile(primeNumber);
             }
         }
+        String countMessage = "There are " + count + " prime numbers under " + num + ".";
+        System.out.println(countMessage);
+        writeFile.writeToFile(countMessage);
+        writeFile.closeFile();
     }
 
 }
--- WriteFile.java
+++ WriteFile.java
@@ -1,22 +1,39 @@
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.IOException;
-
+//writer file
 public class WriteFile
 {
     private String path;
     private boolean appendToFile = false;
+    private PrintWriter printLine;
 
     public WriteFile(String filePath)
     {
         path = filePath;
-        public void writeToFile
+
     }
 
     public WriteFile(String filePath, boolean appendValue)
     {
         path = filePath;
         appendToFile = appendValue;
+    }
+    public void openFile() throws IOException
+    {
+        FileWriter write = new FileWriter(path, appendToFile);
+        printLine = new PrintWriter(write);
+    }
+    public void closeFile()
+    {
+        if(printLine != null)
+        {
+            printLine.close();
+        }
+    }
+    public void writeToFile(String textLine)
+    {
+        printLine.printf("%s%n", textLine);
     }
 
 
