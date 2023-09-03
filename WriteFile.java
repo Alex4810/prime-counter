9ca83b94ae961d86bfb4c065023a26bc4f8c4324
Alex <alexanderlinus@gmail.com>
2023-09-02 19:52:01 -0700

    added WriteFile

---
+++ WriteFile.java
@@ -0,0 +1,26 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+public class WriteFile
+{
+    private String path;
+    private boolean appendToFile = false;
+
+    public WriteFile(String filePath)
+    {
+        path = filePath;
+        public void writeToFile
+    }
+
+    public WriteFile(String filePath, boolean appendValue)
+    {
+        path = filePath;
+        appendToFile = appendValue;
+    }
+
+
+
+
+
+}
